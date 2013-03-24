43c21ad30cda4d1d9dcf00fb6a1259b27c0f1cbe
rastating <intninety.nospam@gmail.com>
2013-03-24 16:45:56 +0000

    Updated notice

--- jooforce.py
+++ jooforce.py
@@ -1,5 +1,5 @@
 #
-#    Copyright intninety 2012
+#    Copyright rastating 2012-2013
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
